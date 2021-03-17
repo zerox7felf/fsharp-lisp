46c2d080b21db3d0629f20e7e46df8b384364abb
Linus W. Monie <zerox7felf@gmail.com>
2021-03-17 15:32:25 +0100

    Ast things

--- main.fs
+++ main.fs
@@ -2,6 +2,10 @@ namespace FsLisp
 
 module FsLisp = 
 
+    type Error<'a> =
+    | Some of 'a
+    | Error of string
+
     type Const = 
     | Bool of bool
     | Int of int
@@ -10,11 +14,11 @@ module FsLisp =
     | Const of Const
     | Ident of string
 
-    type Error<'a> =
-    | Some of 'a
-    | Error of string
-
     type SymbolTableFunc = Token list -> SymbolTable -> Error<Token>
     and SymbolTable = Map<Ident, SymbolTableFunc>
 
-
+    type AstNode =
+    | Empty
+    | Value of Const
+    | Node of (Ident * AstNode list)
+    | Seq of AstNode list
