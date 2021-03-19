cce33c0b4630fc2bce6c768160bd334d98ed1e10
Linus W. Monie <zerox7felf@gmail.com>
2021-03-19 17:05:02 +0100

    Comments added to parser

--- parser.fs
+++ parser.fs
@@ -11,22 +11,22 @@ module Parser =
     type ParserPosition = (int * int * int) // input index * line nr * column nr
 
     let rec parser
-        input ((index, line, col): ParserPosition) expressionDepth (SymbolTable symbolTable)
+        input ((index, line, col): ParserPosition)  (SymbolTable symbolTable)
         (parsedSubexpressions: ExpressionPart list) currWord : (Error<(AstNode * SymbolTable)> * ParserPosition) =
         // TODO: move parser arguments into a ParserState tuple, so the helper functions can be made shorter...
 
         // Skip over areas of whitespace
-        let rec parseWhiteSpace (input: string) ((index, line, col): ParserPosition) expressionDepth symbolTable parsedSubexpressions =
+        let rec parseWhiteSpace (input: string) ((index, line, col): ParserPosition)  symbolTable parsedSubexpressions =
             if input = "" then
-                parser input (index, line, col) expressionDepth symbolTable parsedSubexpressions ""
+                parser input (index, line, col)  symbolTable parsedSubexpressions ""
             else
                 match input.[0] with
                 | '\n' ->
-                    parseWhiteSpace input.[1..] (index+1, line+1, 0) expressionDepth symbolTable parsedSubexpressions
+                    parseWhiteSpace input.[1..] (index+1, line+1, 0)  symbolTable parsedSubexpressions
                 | _ when Char.IsWhiteSpace(Convert.ToChar(input.[0])) ->
-                    parseWhiteSpace input.[1..] (index+1, line, col+1) expressionDepth symbolTable parsedSubexpressions
+                    parseWhiteSpace input.[1..] (index+1, line, col+1)  symbolTable parsedSubexpressions
                 | _ ->
-                    parser input (index, line, col) expressionDepth symbolTable parsedSubexpressions ""
+                    parser input (index, line, col)  symbolTable parsedSubexpressions ""
 
         // Build a number from an inverted list of number-characters
         let rec buildNumber number numberBuf pos =
@@ -36,10 +36,9 @@ module Parser =
             | _ -> number
 
         // Parse a number and add it to the list of parsedSubexpressions, which is then passed on for parsing the rest of the expression
-        // TODO: parse the right way around
-        let rec parseNumber (input: string) ((index, line, col): ParserPosition) expressionDepth numberBuf symbolTable parsedSubexpressions =
+        let rec parseNumber (input: string) ((index, line, col): ParserPosition)  numberBuf symbolTable parsedSubexpressions =
             if input = "" then
-                parser input (index, line, col) expressionDepth symbolTable (
+                parser input (index, line, col)  symbolTable (
                     parsedSubexpressions@[
                         AstNode(Value(Const(Int(buildNumber 0 numberBuf 0))))
                     ]
@@ -48,23 +47,34 @@ module Parser =
                 match input.[0] with
                 | _ when Char.IsNumber(Convert.ToChar(input.[0])) ->
                      parseNumber
-                         input.[1..] (index+1, line, col+1) expressionDepth
+                         input.[1..] (index+1, line, col+1) 
                          (input.[0] :: numberBuf)
                          symbolTable parsedSubexpressions
                 | _ when Char.IsWhiteSpace(Convert.ToChar(input.[0])) ->
-                    parser input (index, line, col) expressionDepth symbolTable (
+                    parser input (index, line, col)  symbolTable (
                         parsedSubexpressions@[
                             AstNode(Value(Const(Int(buildNumber 0 numberBuf 0))))
                         ]
                     ) ""
                 | ')' ->
-                    parser input (index, line, col) expressionDepth symbolTable (
+                    parser input (index, line, col)  symbolTable (
                         parsedSubexpressions@[
                             AstNode(Value(Const(Int(buildNumber 0 numberBuf 0))))
                         ]
                     ) ""
                 | _ ->
                     (Error ("Unexpected character " + (string input.[0]) + " in number"), (index, line, col))
+
+        // Parse comment
+        let rec parseComment input ((index, line, col): ParserPosition) symbolTable parsedSubexpressions = 
+            if input = "" then
+                parser input (index, line,col) symbolTable parsedSubexpressions ""
+            else
+                match input.[0] with
+                | '\n' ->
+                    parser input (index,line,col) symbolTable parsedSubexpressions ""
+                | _ ->
+                    parseComment input.[1..] (index+1,line,col+1) symbolTable parsedSubexpressions
 
         // Turn string into token
         let makeToken word : Token = 
@@ -97,16 +107,16 @@ module Parser =
             | _ when Char.IsNumber(Convert.ToChar(input.[0])) && currWord = "" ->
                 // ^- && currWord = "" allows for other tokens to contain numerals, so long as they do not begin with them
                 // Numbers
-                parseNumber input (index, line, col) expressionDepth [] (SymbolTable symbolTable) parsedSubexpressions
+                parseNumber input (index, line, col) [] (SymbolTable symbolTable) parsedSubexpressions
             | '(' ->
                 // Subexpression
-                match parser input.[1..] (index+1, line, col+1) (expressionDepth+1) (SymbolTable symbolTable) [] "" with
+                match parser input.[1..] (index+1, line, col+1) (SymbolTable symbolTable) [] "" with
                 | (Error msg, (newIndex, newLine, newCol)) -> (Error msg, (newIndex, line, col))
                 | (ErrSome (astNode, newSymbolTable), (newIndex, newLine, newCol)) ->
                     if (1+newIndex-index) > input.Length then
                         (Error "Unexpected end of file", (index, line, col))
                     else
-                        parser input.[(1+newIndex-index)..] (newIndex+1, newLine, newCol) expressionDepth newSymbolTable (
+                        parser input.[(1+newIndex-index)..] (newIndex+1, newLine, newCol)  newSymbolTable (
                             if currWord = "" then
                                 parsedSubexpressions@[AstNode astNode]
                             else
@@ -127,17 +137,20 @@ module Parser =
             | _ when Char.IsWhiteSpace(Convert.ToChar(input.[0])) ->
                 // End of word
                 if currWord = "" then
-                    parseWhiteSpace input (index, line, col) expressionDepth (SymbolTable symbolTable) parsedSubexpressions
-                else 
-                    parseWhiteSpace input.[1..] (index+1, line, col+1) expressionDepth (SymbolTable symbolTable) (parsedSubexpressions@[Token(makeToken currWord)])
+                    parseWhiteSpace input (index, line, col)  (SymbolTable symbolTable) parsedSubexpressions
+                else if currWord.Length >= 2 && currWord.[..1] = "//" then
+                    //Comment
+                    parseComment input (index, line, col) (SymbolTable symbolTable) parsedSubexpressions
+                else
+                    parseWhiteSpace input.[1..] (index+1, line, col+1)  (SymbolTable symbolTable) (parsedSubexpressions@[Token(makeToken currWord)])
             | _ ->
                 // Other token (either keyword or identifier)
-                parser input.[1..] (index+1, line, col+1) expressionDepth (SymbolTable symbolTable) parsedSubexpressions (currWord + (string input.[0]))
+                parser input.[1..] (index+1, line, col+1)  (SymbolTable symbolTable) parsedSubexpressions (currWord + (string input.[0]))
 
     let parse input symbolTable =
         if input = "" then
             (Error "Unexpected end of file", (0,0,0))
         else if input.[0] = '(' then
-            parser input.[1..] (0,0,0) 0 symbolTable [] ""
+            parser input.[1..] (0,0,0) symbolTable [] ""
         else
             (Error "Expected start of expression at beginning of file", (0,0,0))
